3b8b26a33315a5898146749b2c1a1bec167541bf
Thiago Bezerra <101569921+Thiagobezerrabarbosa@users.noreply.github.com>
2022-07-05 13:00:28 -0300

    Add files via upload

---
+++ Aula1/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        //calculadora
+            System.out.println("exercicio calculadora");
+            Calculadora.soma(3, 6);
+            Calculadora.subtracao(9, 1.8);
+            Calculadora.multiplicacao(7, 8);
+            Calculadora.divisao(5, 2.5);
+    }
+}
